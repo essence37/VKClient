e6bc8c4a20073ae996299969b725ffb07b82c41c
essence37 <pazin.d@icloud.com>
2019-11-03 08:30:59 +0300

    Добавил view для тени и отображения круга. Написал код для отображения круглой рамки.

---
+++ VKClient/VKClient/View/UserImageShadow.swift
@@ -0,0 +1,13 @@
+//
+//  UserImageShadow.swift
+//  VKClient
+//
+//  Created by Пазин Даниил on 02.11.2019.
+//  Copyright © 2019 Пазин Даниил. All rights reserved.
+//
+
+import UIKit
+
+class UserImageShadow: UIView {
+    
+}
---
+++ VKClient/VKClient/View/UserImageView.swift
@@ -0,0 +1,13 @@
+//
+//  UserImageView.swift
+//  VKClient
+//
+//  Created by Пазин Даниил on 02.11.2019.
+//  Copyright © 2019 Пазин Даниил. All rights reserved.
+//
+
+import UIKit
+
+class UserImageView: UIView {
+    
+}
