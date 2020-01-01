642ce66ee7a979a760ae552bd978dbbec18aebd0
essence37 <pazin.d@icloud.com>
2020-01-01 14:54:23 +0300

    Добавил тень.

---
+++ VKClient/VKClient/View/LoadView.swift
@@ -0,0 +1,21 @@
+//
+//  LoadView.swift
+//  VKClient
+//
+//  Created by Пазин Даниил on 23.11.2019.
+//  Copyright © 2019 Пазин Даниил. All rights reserved.
+//
+
+import UIKit
+
+class LoadView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ VKClient/VKClient/View/NewsViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsViewCell.swift
+//  VKClient
+//
+//  Created by Пазин Даниил on 21.11.2019.
+//  Copyright © 2019 Пазин Даниил. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
